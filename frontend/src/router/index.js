420cd6588cc636a8601c2b1dac9cb5b3c20f838b
전병훈 <happycat_91@naver.com>
2024-01-02 05:32:15 +0000

    vue 설치및 라우팅 설정

---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+// src/router/index.js
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        }
+    ]
+})
